0f2aacefc4c3695e0ddc12008fa192d6d4ad239c
court-tek <dishoughng@gmail.com>
2024-02-17 13:33:49 -0500

    initial commit

---
+++ App/views/partials/top-banner.php
@@ -0,0 +1,7 @@
+<!-- Wrapper -->
+<div id="wrapper">
+
+<!-- Header -->
+    <header id="header">
+        <a href="index.html" class="logo">Massively</a>
+    </header>
---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => '',
+    'username' => 'root',
+    'password' => '',
+];
+
+
